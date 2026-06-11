0842eb2586d541967d836abb12a906931a1b408c
Enochkamsuan <enoch.liangtuang@gmail.com>
2026-06-11 12:32:15 +0530

    header completed

---
+++ app/navigation/header.jsx
@@ -0,0 +1,104 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import logo from "../assets/images/nsdmalogo.png";
+
+const navigation = [
+  { name: "Forecast", href: "#", current: true },
+  { name: "About", href: "#", current: false },
+  { name: "Weather Buletin", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+  return (
+    <Disclosure
+      as="nav"
+      className="relative bg-white md:py-3 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+    >
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block size-6 group-data-open:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden size-6 group-data-open:block"
+              />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-between">
+            <div className="shrink-0 items-center block sm:hidden">
+              <Image src={logo} width={300} height={200} alt="logo" />
+            </div>
+
+            <div className="shrink-0 items-center hidden sm:block">
+              <Image src={logo} width={600} height={900} alt="logo" />
+            </div>
+
+            <div className="hidden sm:block">
+              <div className="flex space-x-8">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-950/50 text-gray-300"
+                        : "text-red-600 hover:bg-white/5",
+                      "rounded-md px-3 py-2 text-sm font-medium",
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <DisclosurePanel
+        transition
+        className="
+    fixed
+    top-0
+    left-0
+    z-50
+    h-screen
+    w-72
+    bg-[#051937]
+    shadow-xl
+    sm:hidden
+    transition duration-300 ease-out
+    data-closed:-translate-x-full
+  "
+      >
+        <div className="pt-20 px-4 space-y-2">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              className="block rounded-md px-3 py-2 text-base font-medium text-red-600 hover:bg-gray-100"
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+}
